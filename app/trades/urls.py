034857d5cefd660046bd9987fb88f9b1c0361b75
Max Wilkinson <maxwilkinson99@gmail.com>
2020-02-12 13:43:12 +0000

    added report views to urls

--- app/trades/urls.py
+++ app/trades/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('trades/', views.ListCreateDerivataveTrade.as_view()),    
     path('trades/<trade_id>/', views.RetrieveUpdateDestroyDerivataveTrade.as_view()),    
+    path('reports/', views.RetriveReports.as_view()),
+    path('reports/generate/', views.generate_report),
 ]
