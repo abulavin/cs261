7eccb6b0dcedbd1ddc79f5eeb55246e5a4ee3eba
Max Wilkinson <maxwilkinson99@gmail.com>
2020-02-13 15:32:10 +0000

    updated report upload path

--- backend/trades/models.py
+++ backend/trades/models.py
@@ -103,7 +103,7 @@ class Report(models.Model):
     Model to store a report along with the date it was generated.
     """
     def get_upload_path(self, filename):
-        return os.path.join('reports/', self.date_generated, '.pdf')
+        return os.path.join('reports/', self.date_generated, self.id, '.pdf')
 
     date = models.DateField(auto_now_add=True)
     report = models.FileField(upload_to=get_upload_path)
