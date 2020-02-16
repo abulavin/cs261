20b34f1e19573df0b6341180eaa7794a65092840
Yalun-Zhang <yalun.zhang@warwick.ac.uk>
2020-02-16 19:21:16 +0000

    created routing framework

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2> This will be the home page!</h2>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
