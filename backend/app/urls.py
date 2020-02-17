d461c47a361dec3fae66884c6c502b9d938399eb
Max Wilkinson <maxwilkinson99@gmail.com>
2020-02-17 21:16:44 +0000

    added routing for new apps

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trades.urls')),
+    path('', include('report.urls')),
+    path('', include('core.urls')),
 ]
