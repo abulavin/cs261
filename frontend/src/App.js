fc0ce20fed9c6a15e2d80121702a355b55c4b2ab
Yalun-Zhang <yalun.zhang@warwick.ac.uk>
2020-02-17 00:53:08 +0000

    small style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,20 +12,25 @@ import Reports from "./Reports";
 // import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
   return (
     <HashRouter>
       <div className="App">
+        {/* <ul className = "iconsnav">
+          <li><NavLink exact to="/">Global Settings</NavLink></li>
+          <li><NavLink exact to="/">Submit Feedback</NavLink></li>
+          <li><NavLink exact to="/">User Guide</NavLink></li>
+        </ul> */}
+
         <h1>Derivative Trade Monitoring System</h1>
         <ul className = "header">
-          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink to="/NewTrade">New Trade</NavLink></li>
           <li><NavLink to="/Trades">Edit/Delete/View Trades</NavLink></li>
           <li><NavLink to="/Reports">View Reports</NavLink></li>
         </ul>
 
-        <div className = "Content">
+        <div className = "content">
           <Route exact path="/" component={Home}/>
           <Route path="/NewTrade" component={NewTrade}/>
           <Route path="/Trades" component={Trades}/>
