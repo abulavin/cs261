a08172da4aa1fd50dac27ab67a53b94c14af59c8
Max Wilkinson <maxwilkinson99@gmail.com>
2020-02-17 21:45:13 +0000

    updated upload path

--- backend/report/models.py
+++ backend/report/models.py
@@ -6,7 +6,7 @@ class Report(models.Model):
     Model to store a report along with the date it was generated.
     """
     def get_upload_path(self, filename):
-        return os.path.join('reports/', self.date_generated, self.id, '.pdf')
+        return os.path.join('report/reports/', self.date_generated, '-', self.id, '.pdf')
 
     date = models.DateField(auto_now_add=True)
     report = models.FileField(upload_to=get_upload_path)
