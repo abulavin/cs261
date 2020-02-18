c7bfd40822997c74de98aa7ffd11448f83c78449
Max Wilkinson <maxwilkinson99@gmail.com>
2020-02-18 21:53:34 +0000

    added pagination to list trades

--- backend/trades/views.py
+++ backend/trades/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
@@ -6,6 +7,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import DerivativeTradeSerializer
 from .models import DerivativeTrade, DerivativeTradeHistory
 
+
+class DerivativeTradePagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 class ListCreateDerivativeTrade(ListCreateAPIView):
     """
@@ -15,6 +21,7 @@ class ListCreateDerivativeTrade(ListCreateAPIView):
     queryset = DerivativeTrade.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
+    pagination_class = DerivativeTradePagination
 
     def create(self, request, *args, **kwargs):
         """
