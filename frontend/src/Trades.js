8d05ee0c10754d4281726a044610293a26052f19
Yalun-Zhang <yalun.zhang@warwick.ac.uk>
2020-02-21 16:19:25 +0000

    added template for populating trade table

--- frontend/src/Trades.js
+++ frontend/src/Trades.js
@@ -1,13 +1,51 @@
 import React, { Component } from "react";
  
 class Trades extends Component {
+  constructor(props){
+    super(props);
+    this.getHeader = this.getHeader.bind(this);
+    this.getRowsData = this.getRowsData.bind(this);
+    this.getKeys = this.getKeys.bind(this);
+  }
+  // use this function to get the table heading values
+  getKeys = function(){
+
+  }
+  // use this function to populate header with headings
+  getHeader = function(){
+  
+  }
+  // use this function to iterate through the json and return body part of the table
+  getRowsData = function(){
+  
+  }
+
   render() {
     return (
-      <div>
-        <h2> This will be the editing/deleting/viewing trades page!</h2>
-      </div>
+      <React.Fragment>
+        <div>
+          <h2> This will be the editing/deleting/viewing trades page!</h2>
+        </div>
+
+        <div className="tradetable">
+          <table>
+            <thead>
+              <tr>Headings from backend{this.getHeader()}</tr>
+            </thead>
+            <tbody>
+              Rows from backend{this.getRowsData()}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
     );
   }
+
+}
+// use this to return individual rows of the table
+const RenderRow = (props) =>{
+  
 }
  
 export default Trades;
+
