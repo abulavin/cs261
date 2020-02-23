12121943b43891ee28fa27bc21ccf42aa7b81a8e
abulavin <artemy@live.co.uk>
2020-02-23 12:45:35 +0000

    trade validator and unit tests

---
+++ frontend/src/TradeValidator.js
@@ -0,0 +1,223 @@
+/**
+ * Utility module for validating trades
+ * @module TradeValidator
+ */
+import currencyCodes from './currencyCodes';
+
+export const TradeValidator = {
+
+    tradeProperties: [
+        'date_of_trade',
+        'trade_id',
+        'product',
+        'buying_party',
+        'selling_party',
+        'notational_amount',
+        'quantity',
+        'notational_currency',
+        'maturity_date',
+        'underlying_price',
+        'underlying_currency',
+        'strike_price'
+    ],
+
+    /**
+     * Validates the fields of the derivative trade one-by-one to be valid at basic level.
+     * Throws an exception if any fields are invalid.
+     * @param {Object} trade Object representing a trade entry
+     * @alias module:TradeValidator
+     */
+    validateTrade: function (trade) {
+        try {
+            if (trade !== undefined) {
+                this.tradeHasAllNecessaryProperties(trade);
+                this.tradeHasNoUndefinedProperties(trade);
+                if (!this.tradeIDisNumerical(trade.trade_id))
+                    this.throwError("Invalid Trade ID: " + trade.trade_id);
+
+                if (!this.dateOfTradeIsValid(trade.date_of_trade))
+                    this.throwError("Invalid trade date: " + trade.date_of_trade);
+
+                if (!this.dateOfTradeIsValid(trade.maturity_date))
+                    this.throwError("Invalid trade date: " + trade.maturity_date);
+
+                if (!this.stringLengthIsValid(trade.buying_party))
+                    this.throwError("Invalid buying party: ", + trade.buying_party);
+
+                if (!this.stringLengthIsValid(trade.selling_party))
+                    this.throwError("Invalid selling party: " + trade.selling_party);
+
+                if (!this.stringLengthIsValid(trade.product))
+                    this.throwError("Invalid product description: " + trade.product);
+
+                if (!this.productPriceIsValid(trade.notational_amount))
+                    this.throwError("Invalid notional amount: " + trade.notational_amount);
+
+                if (!this.productPriceIsValid(trade.underlying_price))
+                    this.throwError("Invalid underlying price amount: " + trade.underlying_price);
+
+                if (!this.productPriceIsValid(trade.strike_price))
+                    this.throwError("Invalid strike price amount: " + trade.strike_price);
+
+                if (!this.productQuantityIsValid(trade.quantity))
+                    this.throwError("Invalid trade quantity: " + trade.quantity);
+
+                if (!this.currencyCodeIsValid(trade.notational_currency))
+                    this.throwError("Invalid notional currency code: " + trade.notational_currency);
+
+                if (!this.currencyCodeIsValid(trade.underlying_currency))
+                    this.throwError("Invalid underlying currency code: " + trade.underlying_currency);
+
+                return true;
+            } else {
+                this.throwError("Input trade is undefined.");
+            }
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    /**
+     * Returns true if the trade has all attributes necessary.
+     * Throws an exception with an error listing the missing fields otherwise.
+     * @param {Object} trade Derivative trade
+     * @alias module:TradeValidator
+     */
+    tradeHasAllNecessaryProperties: function (trade) {
+        let missingProperties = [];
+        this.tradeProperties.forEach(property => {
+            if (!trade.hasOwnProperty(property))
+                missingProperties.push(property);
+        });
+        if (missingProperties.length > 0) {
+            let errorMessage = "Trade has missing properties: ";
+            missingProperties.forEach(property => {
+                errorMessage += property + ", ";
+            });
+            throw new Error(errorMessage);
+        }
+        return true;
+    },
+
+    /**
+     * Returns true if all the attributes of the input object are defined
+     *  i.e no attributes have the value `undefined`.
+     * Throws an exception listing undefined attributes otherwise.
+     * Note: Will return true for the empty object {}.
+     * @param {Object} trade Derivative trade
+     * @alias module:TradeValidator
+     */
+    tradeHasNoUndefinedProperties: function (trade) {
+        let unidentifiedAttributes = [];
+        for (const attribute in trade) {
+            if (trade[attribute] === undefined)
+                unidentifiedAttributes.push(attribute);
+        }
+        if (unidentifiedAttributes.length > 0) {
+            let errorMessage = "Trade has undefined properties: ";
+            unidentifiedAttributes.forEach(property => {
+                errorMessage += property + ", ";
+            });
+            throw new Error(errorMessage);
+        }
+        return true;
+    },
+
+    /**
+     * Returns true if the object has all the necessary trade attributes.
+     * Similar to `tradeHasAllNecessaryProperties`, except returns false
+     * if an attribute is not found instead of throwing an exception.
+     * @param {Object} trade derivative trade
+     * @alias module:TradeValidator
+     */
+    checkTradeHasAllProperties: function (trade) {
+        for (const property in this.tradeProperties) {
+            if (!trade.hasOwnProperty(property)) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    /**
+     * Returns true of all the attributes of the input object are defined 
+     * i.e none of the attributes of the object have the value `undefined`
+     * or the object is empty ({}).
+     * Returns false otherwise.
+     * @param {Object} trade Derivative trade
+     * @alias module:TradeValidator
+     */
+    checkTradeFullyDefined: function (trade) {
+        for (let attribute in trade) {
+            if (trade[attribute] === undefined)
+                return false;
+        }
+        return true;
+    },
+
+    /**
+     * Returns true if the trade ID matches a set of digits [0-9].
+     * Does not check if the trade ID exists in the database
+     * @param {number} tradeID Trade ID number
+     * @alias module:TradeValidator
+     */
+    tradeIDisNumerical: function (tradeID) {
+        const regex = /^[0-9]+$/;
+        return regex.test(tradeID);
+    },
+
+    /**
+     * Returns true if the input string can successfully be parsed into a date.
+     * False otherwise.
+     * @param {string} date Date string representation
+     * @alias module:TradeValidator
+     */
+    dateOfTradeIsValid: function (date) {
+        return !!Date.parse(date);
+    },
+
+    /**
+     * Returns true if the quantity value can be parsed into an integer
+     * and is non-zero.
+     * @param {number} quantity 
+     * @alias module:TradeValidator
+     */
+    productQuantityIsValid: function (quantity) {
+        let parsedQuantity = parseFloat(quantity, 10);
+        return !Number.isNaN(parsedQuantity) && Number.isInteger(parsedQuantity) && parsedQuantity > 0;
+    },
+
+    /**
+     * Returns true if the price value can be parsed into a float and is non-zero.
+     * @param {number} price 
+     * @alias module:TradeValidator
+     */
+    productPriceIsValid: function (price) {
+        let parsedPrice = parseFloat(price);
+        return !Number.isNaN(parsedPrice) && parsedPrice > 0;
+    },
+
+    /**
+     * Returns true if the string value is defined, non-empty 
+     * and at most 200 chars in length.
+     * @param {string} stringField 
+     * @alias module:TradeValidator
+     */
+    stringLengthIsValid: function (stringField) {
+        return stringField && stringField.length > 0 && stringField.length <= 200;
+    },
+
+    /**
+     * Returns true if the input code is included in the set of currencies 
+     * provided in the test data.
+     * @param {string} code 3 Letter ISO currency code
+     * @alias module:TradeValidator
+     */
+    currencyCodeIsValid: function (code) {
+        return currencyCodes.has(code);
+    },
+
+    throwError: function (message) {
+        throw new Error(message);
+    }
+}
---
+++ frontend/src/__tests__/TradeValidator.test.js
@@ -0,0 +1,161 @@
+import TradeValidator from '../TradeValidator';
+import currencyCodes from '../currencyCodes';
+
+const exampleTrade = {
+    date_of_trade: "2020-02-02",
+    trade_id: "1",
+    product: "1",
+    buying_party: "1",
+    selling_party: "1",
+    notational_amount: 1.0,
+    quantity: 1.0,
+    notational_currency: "USD",
+    maturity_date: "2020-02-20",
+    underlying_price: 1.0,
+    underlying_currency: "USD",
+    strike_price: 1.0
+};
+const wrongTrade = {
+    date_of_trade: undefined,
+    trade_id: "1",
+    product: "1",
+    buying_party: "1",
+    selling_party: "1",
+    notational_amount: 1.0,
+    quantity: 1.0,
+    notational_currency: "1",
+    maturity_date: undefined,
+    underlying_price: 1.0,
+    underlying_currency: "1"
+};
+
+const badTrades = [
+    {},
+    {
+        date_of_trade: "2020-02-02",
+        trade_id: "1",
+        product: "1",
+        quantity: undefined
+    },
+    wrongTrade
+]
+
+test('TradeValidator identifies valid IDs', () => {
+    const maxIDRange = 20000000;
+    for (let i = 0; i < 10000; i++) {
+        let rand = Math.ceil(Math.random() * maxIDRange);
+        expect(TradeValidator.tradeIDisNumerical(rand)).toBeTruthy();
+        expect(TradeValidator.tradeIDisNumerical(rand + "")).toBeTruthy();
+    }
+});
+
+test('TradeValidator identifies invalid IDs', () => {
+    expect(TradeValidator.tradeIDisNumerical("")).toBeFalsy();
+    expect(TradeValidator.tradeIDisNumerical("Forty two")).toBeFalsy();
+    expect(TradeValidator.tradeIDisNumerical(true)).toBeFalsy();
+    expect(TradeValidator.tradeIDisNumerical(3.141)).toBeFalsy();
+    expect(TradeValidator.tradeIDisNumerical(undefined)).toBeFalsy();
+});
+
+test('TradeValidator proxy identifies a complete trade object', () => {
+    expect(TradeValidator.tradeHasAllNecessaryProperties(exampleTrade)).toBeTruthy();
+})
+
+test('TradeValidator identifies an incomplete trade object', () => {
+    badTrades.forEach(trade => {
+        expect(() => {
+            TradeValidator.tradeHasAllNecessaryProperties(trade);
+        }).toThrow();
+    })
+});
+
+test('TradeValidator rejects an invalid trade object', () => {
+    badTrades.forEach(trade => {
+        expect(() => {
+            TradeValidator.validateTrade(trade);
+        }).toThrow();
+    })
+});
+
+test('TradeValidator accepts a trade object', () => {
+    expect(TradeValidator.validateTrade(exampleTrade)).toBeTruthy();
+})
+
+test('TradeValidator rejects an undefined trade', () => {
+    expect(() => {
+        TradeValidator.validateTrade(undefined)
+    }).toThrow("Input trade is undefined.");
+});
+
+test('TradeValidator identifies undefined trade attributes', () => {
+    expect(TradeValidator.tradeHasNoUndefinedProperties(exampleTrade)).toBeTruthy();
+    expect(() => {
+        TradeValidator.tradeHasNoUndefinedProperties(wrongTrade);
+    }).toThrow();
+});
+
+test('TradeValidator returns false on trade with undefined attributes', () => {
+    expect(TradeValidator.checkTradeFullyDefined(wrongTrade)).toBeFalsy();
+});
+
+test('TradeValidator returns false on trade with missing attributes', () => {
+    expect(TradeValidator.checkTradeHasAllProperties(wrongTrade)).toBeFalsy();
+    expect(TradeValidator.checkTradeHasAllProperties({})).toBeFalsy();
+});
+
+test('TradeValidator identifies a valid a date', () => {
+    expect(TradeValidator.dateOfTradeIsValid('2012-02-02')).toBeTruthy();
+    expect(TradeValidator.dateOfTradeIsValid('28th July 2000')).toBeFalsy();
+    expect(TradeValidator.dateOfTradeIsValid(298572985)).toBeFalsy();
+    expect(TradeValidator.dateOfTradeIsValid('298572985')).toBeFalsy();
+    expect(TradeValidator.dateOfTradeIsValid('')).toBeFalsy();
+    expect(TradeValidator.dateOfTradeIsValid(undefined)).toBeFalsy();
+});
+
+test('TradeValidator correctly identifies valid product prices', () => {
+    const maxRange = 20000000;
+    for (let i = 0; i < 10000; i++) {
+        let randFloat = Math.random() * maxRange;
+        let rand = Math.ceil(randFloat);
+        expect(TradeValidator.productPriceIsValid(rand)).toBeTruthy();
+        expect(TradeValidator.productPriceIsValid(rand + "")).toBeTruthy();
+        expect(TradeValidator.productPriceIsValid(randFloat)).toBeTruthy();
+        expect(TradeValidator.productPriceIsValid(randFloat + "")).toBeTruthy();
+    }
+    expect(TradeValidator.productPriceIsValid(0)).toBeFalsy();
+    expect(TradeValidator.productPriceIsValid("1")).toBeTruthy();
+    expect(TradeValidator.productPriceIsValid("Forty two")).toBeFalsy();
+    expect(TradeValidator.productPriceIsValid(undefined)).toBeFalsy();
+});
+
+test('TradeValidator correctly identifies valid product quantities', () => {
+    const maxRange = 20000000;
+    for (let i = 0; i < 10000; i++) {
+        let rand = Math.ceil(Math.random() * maxRange);
+        expect(TradeValidator.productQuantityIsValid(rand)).toBeTruthy();
+        expect(TradeValidator.productQuantityIsValid(rand + "")).toBeTruthy();
+    }
+    expect(TradeValidator.productQuantityIsValid(0)).toBeFalsy();
+    expect(TradeValidator.productQuantityIsValid("Forty two")).toBeFalsy();
+    expect(TradeValidator.productQuantityIsValid(undefined)).toBeFalsy();
+    expect(TradeValidator.productQuantityIsValid("1")).toBeTruthy();
+});
+
+test('TradeValidator identifies invalid party names', () => {
+    let longString = "";
+    for (let i = 0; i < 200; i++) {
+        longString += "a";
+    }
+    expect(TradeValidator.stringLengthIsValid("")).toBeFalsy();
+    expect(TradeValidator.stringLengthIsValid(longString)).toBeTruthy();
+    expect(TradeValidator.stringLengthIsValid(longString + "a")).toBeFalsy();
+    expect(TradeValidator.stringLengthIsValid(undefined)).toBeFalsy();
+    expect(TradeValidator.stringLengthIsValid("My name Jeff")).toBeTruthy();
+});
+
+test('TradeValidator recognises valid currency codes', () => {
+    for (let code in currencyCodes) {
+        expect(TradeValidator.currencyCodeIsValid(code)).toBeTruthy();
+    }
+    expect(TradeValidator.currencyCodeIsValid("djhfjdhg")).toBeFalsy();
+});
