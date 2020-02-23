30da4bb2314dff28b46b1664e0780aad78d6b6a0
abulavin <artemy@live.co.uk>
2020-02-23 21:07:28 +0000

    Basic test interaction

--- frontend/src/NewTrade.js
+++ frontend/src/NewTrade.js
@@ -3,30 +3,35 @@ import {CreateTradeProxy} from "./BackendProxy";
  
 class NewTrade extends Component {
 
-  sendCreatePostRequest() {
-    const exampleTrade =   {
-      date_of_trade: "2020-02-02",
-      trade_id: "1",
-      product: "1",
-      buying_party: "1",
-      selling_party: "1",
-      notational_amount: 1.0,
-      quantity: 1.0,
-      notational_currency: "1",
-      maturity_date: "2020-02-20",
-      underlying_price: 1.0,
-      underlying_currency: "1",
-      strike_price: 1.0
-  };
-    let proxy = new CreateTradeProxy();
-    proxy.createTrade(exampleTrade);
+  constructor(props) {
+    super(props);
+
+    this.props.createProxy = new CreateTradeProxy();
+  }
+
+  sendTrade = (trade) => {
+      trade = {
+        date_of_trade: "2020-02-02",
+        trade_id: "TEST101",
+        product: "1",
+        buying_party: "1",
+        selling_party: "1",
+        notational_amount: 1.0,
+        quantity: 1.0,
+        notational_currency: "USD",
+        maturity_date: "2020-02-20",
+        underlying_price: 1.0,
+        underlying_currency: "USD",
+        strike_price: 1.0
+    };
+    this.props.createProxy.createTrade(trade);
   }
 
   render() {
     return (
       <div>
         <h2> This will be the trade creation page!</h2>
-        <button className='NavBtn' onClick={this.sendCreatePostRequest}>Send</button>
+        <button className='NavBtn' onClick={this.sendTrade}>Send</button>
       </div>
     );
   }
--- frontend/src/Trades.js
+++ frontend/src/Trades.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
-import { GetTradeProxy } from "./BackendProxy";
- 
+import { GetTradeProxy, DeleteTradeProxy } from "./BackendProxy";
+
 class Trades extends Component {
 
-  getTrades() {
-    let proxy = new GetTradeProxy();
-    console.log(proxy.getListOfTrades());
+  constructor(props) {
+    super(props);
+    this.props.getProxy = new GetTradeProxy();
+    this.props.deleteProxy = new DeleteTradeProxy();
+  }
+
+  getTrades = () => {
+    this.props.getProxy.getListOfTrades()
+      .then(trades => console.log(trades))
+      .catch(error => { throw error });
+  }
+
+  getTradesByID = (tradeID) => {
+    tradeID = "TEST101";
+    this.props.getProxy.getTradeByID(tradeID)
+      .then(trade => console.log(trade));
+  }
+
+  deleteTradeByID = (tradeID) => {
+    tradeID = "TEST101";
+    this.props.deleteProxy.deleteTrade(tradeID);
   }
 
   render() {
     return (
       <div>
         <h2> This will be the editing/deleting/viewing trades page!</h2>
-        <div onClick={this.getTrades()}>Get Trades</div>
+        <button onClick={this.getTrades}>Get Trades</button>
+        <button onClick={this.getTradesByID}>Get Trade TEST101</button>
+        <button onClick={this.deleteTradeByID}>Delete TEST101</button>
       </div>
     );
   }
 }
- 
+
 export default Trades;
