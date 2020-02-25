b2084710794e636657893411ef3af38c9936d520
Yalun-Zhang <yalun.zhang@warwick.ac.uk>
2020-02-25 18:54:25 +0000

    added settings toggle for error module

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -3,9 +3,19 @@ import React, { Component } from "react";
 class Settings extends Component {
   render() {
     return (
-      <div>
-        <h2> This will be the settings page!</h2>
-      </div>
+      <React.Fragment>
+        <div>
+          <h2> Use this page to change settings.</h2>
+          <h4 id="settingsname">Error Correction Module:  </h4>
+          <div class="toggle-switch">
+            <input type="checkbox" class="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
+            <label class="toggle-switch-label" for="toggleSwitch">
+              <span class="toggle-switch-inner"></span>
+              <span class="toggle-switch-switch"></span>
+            </label>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
