90717127a6ddbcffc07236be674981d5c6b72221
Yalun Zhang <yalun.zhang@warwick.ac.uk>
2020-03-01 17:15:04 +0000

    created table for fetching reports

--- frontend/src/Reports.js
+++ frontend/src/Reports.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import ReportTable from './Components/ReportTable.js';
 import { GetReportProxy } from "./BackendProxy";
 import {Label } from "reactstrap";
-import ReportModal from './Components/ReportModal.js';
+
 
 class Reports extends Component {
   constructor(props){
     super(props);
-    this.getHeader = this.getHeader.bind(this);
-    this.getRowsData = this.getRowsData.bind(this);
-    this.getKeys = this.getKeys.bind(this);
     this.reportProxy = new GetReportProxy();
+    this.state = {
+      rep: [],
+      count: 2
+    }
+  }
+
+  componentDidMount() {
+    this.reportProxy.getListOfReports()
+      .then(reports => {
+        this.setState({rep: reports.results})
+        console.log(reports)
+      })
   }
 
   getListOfReports = () => {
@@ -31,65 +41,27 @@ class Reports extends Component {
           .then(reports => console.log(reports));
   }
 
-  // use this function to get the table heading values
-  getKeys = function(){
-
-  }
-
-  // use this function to iterate through the json and return body part of the table
-  getRowsData = function(){
-
-  }
   render() {
     return (
-
       <React.Fragment>
-      <div>
-        <h2> Use this page to view and download reports.</h2>
-        <button onClick={this.getListOfReports}>Get reports</button>
-        <button onClick={this.getReportsAfter}>Get After 2020-02-28</button>
-        <button onClick={this.getReportsBefore}>Get reports before 2020-02-28</button>
-        <button onClick={this.getReportsOn}>Get reports on 2020-02-28</button>
-      </div>
+        <div>
+          <h2> Use this page to view and download reports.</h2>
+          <button onClick={this.getListOfReports}>Get reports</button>
+          <button onClick={this.getReportsAfter}>Get After 2020-02-28</button>
+          <button onClick={this.getReportsBefore}>Get reports before 2020-02-28</button>
+          <button onClick={this.getReportsOn}>Get reports on 2020-02-28</button>
+        </div>
 
-      <div className="reportoptions">
-        <Label>Filter by: </Label>
-              <select id="heading" name="heading">
-                <option value="heading">Heading1</option>
-              </select>
-      </div>
-      <div className="reporttable">
-        {/* onkeyup search for item function */}
-        <input type="text" id="searchinput" onkeyup="" placeholder="Search for .."></input>
-        <table>
-          <thead>
-            <tr>Headings from backend{this.getHeader()}
-                <th>View</th>
-                <th>Download</th>
-            </tr>
-            </thead>
-            <tbody>
-                <tr>Rows from backend{this.getRowsData()}
-                    <td>
-                        <ReportModal/>
-                        {/* if trade is editable, render edit button */}
-                    </td>
-                    <td>
-                        <button > Download</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-      </div>
-
-
+        <div className="reportoptions">
+          <Label>Filter by: </Label>
+                <select id="heading" name="heading">
+                  <option value="heading">Heading1</option>
+                </select>
+        </div>
+        {this.state.rep ? <ReportTable data={this.state.rep}/> : null }
+  
     </React.Fragment>
     );
   }
-}
-
-// use this to return individual rows of the table
-const RenderRow = (props) =>{
-
 }
 export default Reports;
