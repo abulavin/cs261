24c774bb4aba23c42330d0d506bdef8039be46b1
abulavin <artemy@live.co.uk>
2020-03-01 17:16:19 +0000

    Formatting

--- frontend/src/__tests__/App.test.js
+++ frontend/src/__tests__/App.test.js
@@ -3,7 +3,6 @@ import { shallow } from 'enzyme';
 import EditModal from '../Components/EditModal.js'
 
 test('renders nothing when closed', () => {
+    // This is the failing test - commented out to allow check if GitHub action works
     // expect(shallow(<EditModal onClose={jest.fn()} />)).toMatchSnapshot();
 });
-
-
