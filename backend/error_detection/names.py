b31c98524cc5a8d48d4c06d8fb9e3f9b85e2434e
SiliconSloth <siliconsloth64@gmail.com>
2020-03-05 03:38:16 +0000

    Add comments to names.py

--- backend/error_detection/names.py
+++ backend/error_detection/names.py
@@ -6,17 +6,34 @@ from .db_import import (
 
 
 def detect_similar(query, corpus):
+    """
+    Find a string in the corpus "close" to the query string.
+    Case insensitive.
+
+    @:param corpus: maps lower-case strings to their original-case
+    versions from the database.
+
+    :returns whether or not the query was already in the corpus,
+    and the correction if one is found.
+    """
     if query.lower() in corpus:
         return True, None
 
     matches = difflib.get_close_matches(query.lower(), corpus, n=1)
     if matches:
+        # Return original-case version from the corpus dict,
+        # not the lower-case version.
         return False, corpus[matches[0]]
     else:
         return False, None
 
 
 def detect_name_errors(trade, errors, currencies):
+    """
+    Detect erroneous products, buying and selling parties and currencies in a trade.
+    Corrects errors by finding similar strings in the database.
+    """
+
     products = {seller.product.lower(): seller.product for seller in product_sellers}
     ok, correction = detect_similar(trade.product, products)
     if not ok:
