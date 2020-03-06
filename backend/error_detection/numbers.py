d50a6e39695bc712fce3fae99c99aa7f6933a600
SiliconSloth <siliconsloth64@gmail.com>
2020-03-06 01:27:51 +0000

    Check notional amount based on quantity rather than the other way round

--- backend/error_detection/numbers.py
+++ backend/error_detection/numbers.py
@@ -4,7 +4,7 @@ from .db_import import (
 )
 
 
-def correct_to_range(value, min_v, max_v, threshold):
+def correct_to_range(value, min_v, max_v, threshold, need_int=False):
     min_t = min_v * threshold
     max_t = max_v / threshold
 
@@ -17,39 +17,38 @@ def correct_to_range(value, min_v, max_v, threshold):
 
     correction = value / 10
     if min_t <= correction <= max_t:
+        if need_int and not correction.is_integer():
+            return False, None
         return False, correction
     else:
         return False, None
 
 
-def correct_to_past(value, trades, key, threshold):
+def correct_to_past(value, trades, key, threshold, need_int=False):
     past_values = [key(t) for t in trades]
-    return correct_to_range(value, min(past_values), max(past_values), threshold)
+    return correct_to_range(value, min(past_values), max(past_values), threshold, need_int)
 
 
 def detect_number_errors(trade, threshold, errors):
     product_trades = derivative_trades.filter(product=trade.product)
     if len(product_trades) > 0:
-        notional_ok, correction = correct_to_past(trade.notional_amount, product_trades, lambda t: t.notional_amount, threshold)
-        if not notional_ok:
-            errors.append(Error('notional_amount', correction, "Suspicious notional amount"))
+        quantity_ok, correction = correct_to_past(trade.quantity, product_trades, lambda t: t.quantity, threshold, need_int=True)
+        if not quantity_ok:
+            errors.append(Error('quantity', correction, "Suspicious quantity"))
 
         underlying_ok, correction = correct_to_past(trade.underlying_price, product_trades, lambda t: t.underlying_price, threshold)
         if not underlying_ok:
             errors.append(Error('underlying_price', correction, "Suspicious underlying price"))
     else:
-        notional_ok, underlying_ok = True
+        quantity_ok, underlying_ok = True
 
     if underlying_ok:
         ok, correction = correct_to_range(trade.strike_price, trade.underlying_price, trade.underlying_price, threshold)
         if not ok:
             errors.append(Error('strike_price', correction, "Suspicious strike price"))
 
-        if notional_ok:
-            expected_quantity = trade.notional_amount / trade.underlying_price
-            if trade.quantity != expected_quantity:
-                if expected_quantity.is_integer():
-                    errors.append(Error('quantity', expected_quantity, "Quantity does not match price"))
-                else:
-                    errors.append(Error('notional_amount', trade.underlying_price * trade.quantity,
-                                        "Notional amount does not match quantity"))
+        if quantity_ok:
+            expected_notional = trade.quantity * trade.underlying_price
+            if trade.notional_amount != expected_notional:
+                errors.append(Error('notional_amount', trade.underlying_price * trade.quantity,
+                                    "Notional amount does not match quantity"))
