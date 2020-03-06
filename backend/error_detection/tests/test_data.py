bece518f6d3757b25254a88b6c5dbe2bbfd2d2b9
SiliconSloth <siliconsloth64@gmail.com>
2020-03-06 00:54:40 +0000

    Add sample trades to test database

--- backend/error_detection/tests/test_data.py
+++ backend/error_detection/tests/test_data.py
@@ -1,5 +1,5 @@
 import datetime
-from trades.models import CompanyCode, ProductSeller
+from trades.models import CompanyCode, ProductSeller, DerivativeTrade
 
 today = datetime.datetime(2020, 3, 3)
 
@@ -8,13 +8,13 @@ sample_fields = {
     'product': "Trees",
     'buying_party': "CMZC67",
     'selling_party': "HWJF09",
-    'notional_amount': 1000,
-    'quantity': 1000,
+    'notional_amount': 3302.76,
+    'quantity': 51,
     'notional_currency': "USD",
     'maturity_date': datetime.datetime(2050, 9, 3),
-    'underlying_price': 123,
+    'underlying_price': 64.76,
     'underlying_currency': "USD",
-    'strike_price': 50
+    'strike_price': 63
 }
 
 def populate_db():
@@ -42,4 +42,57 @@ def populate_db():
     ProductSeller.objects.create(
         product="Xylophones",
         company_id="HWJF09"
+    )
+
+    DerivativeTrade.objects.create(
+        date_of_trade=datetime.datetime(2020, 2, 27),
+        product="Trees",
+        buying_party="CMZC67",
+        selling_party="HWJF09",
+        notional_amount=3870.02,
+        quantity=77,
+        notional_currency="USD",
+        maturity_date=datetime.datetime(2050, 9, 3),
+        underlying_price=50.26,
+        underlying_currency="USD",
+        strike_price=60
+    )
+    DerivativeTrade.objects.create(
+        date_of_trade=datetime.datetime(2020, 2, 27),
+        product="Trees",
+        buying_party="CMZC67",
+        selling_party="HWJF09",
+        notional_amount=1480,
+        quantity=20,
+        notional_currency="USD",
+        maturity_date=datetime.datetime(2050, 9, 3),
+        underlying_price=74,
+        underlying_currency="USD",
+        strike_price=60.88
+    )
+    DerivativeTrade.objects.create(
+        date_of_trade=datetime.datetime(2020, 2, 27),
+        product="Trees",
+        buying_party="CMZC67",
+        selling_party="HWJF09",
+        notional_amount=2879.36,
+        quantity=64,
+        notional_currency="USD",
+        maturity_date=datetime.datetime(2050, 9, 3),
+        underlying_price=44.99,
+        underlying_currency="USD",
+        strike_price=50
+    )
+    DerivativeTrade.objects.create(
+        date_of_trade=datetime.datetime(2020, 2, 27),
+        product="Rocks",
+        buying_party="CMZC67",
+        selling_party="HWJF09",
+        notional_amount=5608800000,
+        quantity=45600,
+        notional_currency="USD",
+        maturity_date=datetime.datetime(2050, 9, 3),
+        underlying_price=123000,
+        underlying_currency="USD",
+        strike_price=105000
     )
